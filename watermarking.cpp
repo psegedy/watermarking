e49902d2b6c0d871da5be728919e0b1a513ba79b
Patrik Segedy <psegedy@redhat.com>
2017-04-29 20:24:04 +0200

    add command line arguments

--- watermarking.cpp
+++ watermarking.cpp
@@ -1,11 +1,17 @@
-// #include "watermarking.h"
 #include <vector>
 #include <Magick++.h> 
 #include <iostream>
 #include <cstdlib> //atoi
 #include <cmath>
+#include <regex>
+#include <string>
+#include <tuple>
 
 #define BLK_SIZE 8
+#define OUTPUT "watermarked.jpg"
+#define WM_OUTPUT "extracted.jpg"
+#define QUALITY 100
+#define STRENGTH 24
 
 using namespace std; 
 using namespace Magick;
@@ -15,6 +21,11 @@ template< typename T > using matrix_t = vector< vector<T> >;
 
 // matrix3d_t<T> type
 template< typename T > using matrix3d_t = vector< matrix_t<T> >;
+
+enum arg_enum { arg_help = 0, arg_input, arg_output, arg_watermark_in, arg_watermark_out, arg_strength, arg_quality, arg_embed, arg_extract, arg_attack, arg_stat };
+vector<string> arg_names = { "--help", "--input", "--output", "--wm-input", "--wm-output", "--strength", "--quality", "--embed", "--extract", "--attack", "--statistics" };
+// args_t type for command line arguments
+using args_t = tuple<bool, string, string, string, string, int, int, bool, bool, bool, bool>;
 
 // function declarations
 template< typename T >
@@ -31,6 +42,8 @@ void extract(matrix_t<T> &matrix, vector<T> &watermark, int strength, int size=B
 template< typename T >
 void mat_init(matrix_t<T> &matrix, int size);
 template< typename T >
+void mat_round(matrix_t<T> &matrix);
+template< typename T >
 matrix_t<T> mat_multiply(matrix_t<T> &a, matrix_t<T> &b, int size=BLK_SIZE);
 template< typename T >
 matrix_t<T> mat_transpose(matrix_t<T> &matrix);
@@ -39,8 +52,6 @@ double dot_product(const vector<T> &vec1, const vector<T> &vec2);
 template< typename T >
 double normalize(const vector<T> &vec);
 template< typename T >
-void vector_concat(const vector<T> &vec, vector<T> &result);
-template< typename T >
 void gram_schmidt(matrix_t<T> &matrix, matrix3d_t<T> &q_list, matrix3d_t<T> &r_list);
 template< typename T >
 long double psnr(const matrix_t<T> &orig, const matrix_t<T> &wm);
@@ -48,7 +59,210 @@ template< typename T >
 double nc(const vector<T> &orig, const vector<T> &extr);
 template< typename T > 
 void print(const matrix_t<T> &matrix);
+args_t parse_arguments(int argc, char **argv);
+void print_help();
 
+
+int main(int argc, char **argv)
+{
+    args_t args = parse_arguments(argc, argv);
+    cout << "tuple size: " << tuple_size<decltype(args)>::value << endl;
+
+    cout << arg_names[0] << ": " << get<0>(args) << endl;
+    cout << arg_names[1] << ": " << get<1>(args) << endl;
+    cout << arg_names[2] << ": " << get<2>(args) << endl;
+    cout << arg_names[3] << ": " << get<3>(args) << endl;
+    cout << arg_names[4] << ": " << get<4>(args) << endl;
+    cout << arg_names[5] << ": " << get<5>(args) << endl;
+    cout << arg_names[6] << ": " << get<6>(args) << endl;
+    cout << arg_names[7] << ": " << get<7>(args) << endl;
+    cout << arg_names[8] << ": " << get<8>(args) << endl;
+    cout << arg_names[9] << ": " << get<9>(args) << endl;
+
+    InitializeMagick(*argv);
+
+
+    Image image;
+    // print(matrix);
+    // load input image and watermark
+    image.read(get<arg_input>(args));
+    
+
+    matrix3d_t<double> blocks(image.columns()*image.rows()/(BLK_SIZE*BLK_SIZE));
+    matrix_t<double> matrix(image.columns());
+    matrix_t<double> original(image.columns());
+    
+    matrix3d_t<double> q_list;
+    matrix3d_t<double> r_list;
+    matrix3d_t<double> q_list_out;
+    matrix3d_t<double> r_list_out;
+    matrix = image2matrix(image);
+    original = matrix;
+
+    // print(matrix);
+
+    // --watermark provided
+    if (!get<arg_watermark_in>(args).empty()) {
+        Image watermark;
+        watermark.read(get<arg_watermark_in>(args));
+        matrix_t<double> watermark_m(watermark.columns());
+        vector<double> watermark_v;
+        vector<double> ext_watermark_v;
+        watermark_m = image2matrix(watermark);
+
+        // put watermark to vector
+        for (const auto &row : watermark_m)
+            for (double value : row)
+                watermark_v.emplace_back(value);
+
+        // divide image into blocks
+        blocks = get_blocks(matrix);
+
+        // iterate through all blocks
+        for (int i = 0; i < blocks.size(); ++i) {
+            // QR decomposition usinf gram-schmidt process on all blocks
+            gram_schmidt(blocks[i], q_list, r_list);
+            if (get<arg_embed>(args))
+                // embed watermark in R matrix
+                embed(r_list[i], watermark_v, get<arg_strength>(args), i);
+
+            if (get<arg_extract>(args))
+                // extract watermark from image
+                extract(r_list[i], ext_watermark_v, get<arg_strength>(args));
+
+            // multiply Q and R matrixes
+            blocks[i] = mat_multiply(q_list[i], r_list[i]);
+            // round to 8 bytes
+            mat_round(blocks[i]);
+        }
+
+        // concatenate blocks
+        concat_blocks(matrix, blocks);
+        matrix2image(matrix, image);
+
+        // vector to matrix
+        for (int i = 0, k = 0; i < watermark.columns(); ++i)
+            for (int j = 0; j < watermark.rows(); ++j, ++k)
+                watermark_m[i][j] = ext_watermark_v[k];
+
+        // save extracted watermark
+        matrix2image(watermark_m, watermark);
+        watermark.write(get<arg_watermark_out>(args));
+
+        // --statistics
+        if (get<arg_stat>(args)) {
+            // PSNR and NC
+            cout << "PSNR: " << psnr(original, matrix) << endl;
+            cout << "NC: " << nc(watermark_v, ext_watermark_v) << endl;
+        }
+    }
+    
+    // set compression level
+    image.quality(get<arg_quality>(args));
+    // save watermarked image
+    image.write(get<arg_output>(args));
+
+    return 0;
+}
+
+// FUNCTIONS
+
+void print_help()
+{
+    cout << "HELP MEEE" << endl;
+}
+
+// parse command line arguments
+args_t parse_arguments(int argc, char **argv)
+{
+    args_t args;
+    // set some defaults
+    get<arg_output>(args) = OUTPUT;
+    get<arg_watermark_out>(args) = WM_OUTPUT;
+    get<arg_quality>(args) = QUALITY;
+    get<arg_strength>(args) = STRENGTH;
+    get<arg_embed>(args) = false;
+    get<arg_extract>(args) = false;
+    get<arg_attack>(args) = false;
+    get<arg_stat>(args) = false;
+
+    if (argc == 1) {
+        print_help();
+        exit(2);
+    }
+    // vector of argument regexes 
+    vector<regex> regexes;
+    for (string name : arg_names) {
+        regexes.emplace_back(name);
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        // --help
+        if (regex_match(argv[i], regexes[0])) {
+            print_help();
+            exit(0);
+        }
+        // other arguments
+        for (int j = 1; j < regexes.size(); ++j) {
+            if (regex_match(argv[i], regexes[j])) {
+                if (i < arg_quality && argc < (i+1)) {
+                    print_help();
+                    exit(2);
+                }
+                // write value to tuple
+                switch (j) {
+                    case arg_input :
+                        get<arg_input>(args) = argv[i+1];
+                        break;
+                    case arg_output :
+                        get<arg_output>(args) = argv[i+1];
+                        break;
+                    case arg_watermark_in :
+                        get<arg_watermark_in>(args) = argv[i+1];
+                        break;
+                    case arg_watermark_out :
+                        get<arg_watermark_out>(args) = argv[i+1];
+                    case arg_strength :
+                        get<arg_strength>(args) = atoi(argv[i+1]);
+                        break;
+                    case arg_quality :
+                        get<arg_quality>(args) = atoi(argv[i+1]);
+                        break;
+                    case arg_embed :
+                        get<arg_embed>(args) = true;
+                        break;
+                    case arg_extract :
+                        get<arg_extract>(args) = true;
+                        break;
+                    case arg_attack :
+                        get<arg_attack>(args) = true;
+                        break;
+                    case arg_stat :
+                        get<arg_stat>(args) = true;
+                        break;
+                    default :
+                        break;
+                }
+            }
+        }
+    }
+    if (get<arg_input>(args).empty()) {
+        cerr << "--input <filename> is required" << endl << "use --help for more information" << endl;
+        exit(2);
+    }
+    if (get<arg_stat>(args) == true || get<arg_embed>(args) == true || get<arg_extract>(args) == true) {
+        if (get<arg_watermark_in>(args).empty()) {
+            cerr << "--wm-input is required with --statistics, --embed or --extract" << endl << "use --help for more information" << endl;
+            exit(2);
+        }
+    }
+    if (get<arg_embed>(args) == false && get<arg_extract>(args) == false) {
+        get<arg_embed>(args) = true;
+        get<arg_extract>(args) = true;
+    }
+
+    return args;
+}
 
 // get BLK_SIZE x BLK_SIZE blocks from matrix
 template< typename T >
@@ -69,6 +283,7 @@ matrix3d_t<T> get_blocks(const matrix_t<T> &matrix, int n/*=BLK_SIZE*/)
     return blocks;
 }
 
+// Concatenate blocks
 template< typename T >
 void concat_blocks(matrix_t<T> &result, matrix3d_t<T> &blocks)
 {
@@ -89,7 +304,7 @@ void concat_blocks(matrix_t<T> &result, matrix3d_t<T> &blocks)
     }
 }
 
-// transform Image objet into matrix_t type
+// transform Image object into matrix_t type
 // every value is in range <0-255>
 //   0     - black
 //   255   - white
@@ -112,6 +327,7 @@ matrix_t<double> image2matrix(Image &image)
     return mat_pixels;
 }
 
+// transform matrix of image pixels to Image
 template< typename T>
 void matrix2image(const matrix_t<T> &matrix, Image &image)
 {
@@ -131,7 +347,7 @@ void matrix2image(const matrix_t<T> &matrix, Image &image)
     image.syncPixels();
 }
 
-// TODO: void?, a matrix_t<T> &matrix
+// Watermark embedding algorithm
 template< typename T >
 void embed(matrix_t<T> &matrix, vector<T> &watermark, int strength, int iteration, int size/*=BLK_SIZE*/)
 {
@@ -148,6 +364,7 @@ void embed(matrix_t<T> &matrix, vector<T> &watermark, int strength, int iteratio
     }
 }
 
+// Algorithm for watermark extraction
 template< typename T >
 void extract(matrix_t<T> &matrix, vector<T> &watermark, int strength, int size/*=BLK_SIZE*/)
 {
@@ -163,6 +380,7 @@ void extract(matrix_t<T> &matrix, vector<T> &watermark, int strength, int size/*
     }
 }
 
+// Initialize matrix with zeroes
 template< typename T >
 void mat_init(matrix_t<T> &matrix, int size)
 {
@@ -219,13 +437,6 @@ double normalize(const vector<T> &vec)
     return sqrt(dot_product(vec, vec));
 }
 
-// vector concatenation
-template< typename T >
-void vector_concat(const vector<T> &vec, vector<T> &result)
-{
-    result.insert(result.end(), vec.begin(), vec.end());
-}
-
 // QR decomposition using Gram-Schmidt process
 // https://ocw.mit.edu/courses/mathematics/18-335j-introduction-to-numerical-methods-fall-2010/lecture-notes/MIT18_335JF10_lec10a_hand.pdf
 // A = Q*R
@@ -269,6 +480,7 @@ void gram_schmidt(matrix_t<T> &matrix, matrix3d_t<T> &q_list, matrix3d_t<T> &r_l
     r_list.emplace_back(r);
 }
 
+// round matrix values
 template< typename T >
 void mat_round(matrix_t<T> &matrix)
 {
@@ -310,6 +522,8 @@ double nc(const vector<T> &orig, const vector<T> &extr)
     return sum1/sum2;
 }
 
+// print matrix
+// for debugging purposes
 template< typename T > 
 void print(const matrix_t<T> &matrix)
 {
@@ -319,90 +533,4 @@ void print(const matrix_t<T> &matrix)
         cout << endl;
     }
     cout << "--------------------" << endl;
-}
-
-int main(int argc, char **argv)
-{
-    InitializeMagick(*argv);
-
-    Image image;
-    Image watermark;
-    // print(matrix);
-    // load input image and watermark
-    image.read(argv[1]);
-    watermark.read(argv[2]);
-
-    matrix3d_t<double> blocks(image.columns()*image.rows()/(BLK_SIZE*BLK_SIZE));
-    matrix_t<double> matrix(image.columns());
-    matrix_t<double> original(image.columns());
-    matrix_t<double> watermark_m(watermark.columns());
-    vector<double> watermark_v;
-    vector<double> ext_watermark_v;
-    matrix3d_t<double> q_list;
-    matrix3d_t<double> r_list;
-    matrix3d_t<double> q_list_out;
-    matrix3d_t<double> r_list_out;
-    matrix = image2matrix(image);
-    watermark_m = image2matrix(watermark);
-    original = matrix;
-
-    // put watermark to vector
-    for (const auto &row : watermark_m)
-        for (double value : row)
-            watermark_v.emplace_back(value);
-
-    // print(matrix);
-
-    // divide image into blocks
-    blocks = get_blocks(matrix);
-
-    // iterate through all blocks
-    for (int i = 0; i < blocks.size(); ++i) {
-        // QR decomposition usinf gram-schmidt process on all blocks
-        gram_schmidt(blocks[i], q_list, r_list);
-        
-        // embed watermark in R matrix
-        embed(r_list[i], watermark_v, atoi(argv[3]), i);
-
-        // extract watermark from image
-        extract(r_list[i], ext_watermark_v, atoi(argv[3]));
-
-        // multiply Q and R matrixes
-        blocks[i] = mat_multiply(q_list[i], r_list[i]);
-        // round to 8 bytes
-        mat_round(blocks[i]);
-    }
-    
-
-    // concatenate blocks
-    concat_blocks(matrix, blocks);
-    matrix2image(matrix, image);
-    // image.quality(30);
-    // save watermarked image
-    image.write("watermarked.jpg");
-
-
-    // extract watermark from image
-    // for (auto r : r_list)
-    //     extract(r, ext_watermark_v, atoi(argv[3]));
-
-    // PSNR and NC
-    cout << "psnr: " << psnr(original, matrix) << endl;
-    cout << "nc: " << nc(watermark_v, ext_watermark_v) << endl;
-    int count = 0;
-    for (int i = 0; i < watermark_v.size(); ++i)
-        if (watermark_v[i] != ext_watermark_v[i])
-            count++;
-    cout << "count: " << count << endl;
-
-    // vector to matrix
-    for (int i = 0, k = 0; i < watermark.columns(); ++i)
-        for (int j = 0; j < watermark.rows(); ++j, ++k)
-            watermark_m[i][j] = ext_watermark_v[k];
-
-    // save extracted watermark
-    matrix2image(watermark_m, watermark);
-    watermark.write("ext_watermark.jpg");
-
-    return 0;
 }
